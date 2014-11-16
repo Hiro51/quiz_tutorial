6b55af008c292a72bfaf42e0230988bb58971b2d
Hiro51 <h.tsujino0714@gmail.com>
2014-11-16 21:25:29 +0900

    Initial Commit

---
+++ quiz_tutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  quiz_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/16/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ quiz_tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quiz_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/16/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
