30408a29d509fd3eb6fb301840e63d92660e6920
Hiro51 <h.tsujino0714@gmail.com>
2014-11-16 23:06:39 +0900

    Add two labels and buttons to the view controller which is created by cocoa touch class.

---
+++ quiz_tutorial/QuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuizViewController.h
+//  quiz_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/16/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizViewController : UIViewController
+
+@end
