3a60d3973b4a202c117a7ae4bb2b5e34a8f92053
Hiro51 <h.tsujino0714@gmail.com>
2014-11-17 14:48:44 +0900

    Add some constraints to the labels and buttons

--- quiz_tutorial/AppDelegate.m
+++ quiz_tutorial/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "QuizViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,7 +18,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    QuizViewController *quizVC = [[QuizViewController alloc] init];
+    self.window.rootViewController = quizVC;
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
+    
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
